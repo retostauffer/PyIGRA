13e9dc6fb2e01bc7744bb4d2b54821c4c4a7677a
Reto Stauffer <Reto.Stauffer@uibk.ac.at>
2020-09-29 09:01:14 +0200

    Modified setup.py to remove some warnings

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup
 
 setup(name             = "PyIGRA", # This is the package name
-      version          = "0.2-0",  # Current package version, what else
+      version          = "0.2.post0",  # Current package version, what else
       description      = "Downloading and extracting data from the integrated global radiosonde archive",
       long_description = "No long description necessary",
       classifiers=[
@@ -28,5 +28,7 @@ setup(name             = "PyIGRA", # This is the package name
       packages             = ["PyIGRA"],
       install_requires     = ["ConfigParser"],
       scripts              = ["bin/PyIGRA","bin/PyIGRA_search"],
-      include_package_data = True)
+      include_package_data = True,
+      zip_safe = True)
+
 
